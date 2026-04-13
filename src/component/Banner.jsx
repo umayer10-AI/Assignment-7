558f827b67f0cc99537be23d2df95d20ae2182d5
umayer10-AI <mdumayer10@gmail.com>
2026-04-13 19:45:14 +0600

    add

---
+++ src/component/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+
+const Banner = () => {
+    return (
+        <div className='text-center py-10 space-y-4'>
+            <h2 className='text-3xl lg:text-4xl font-bold'>Friends to keep close in your life</h2>
+            <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br /> relationships that matter most.</p>
+            <div className='flex justify-center items-center'>
+                <button className='btn bg-[#244D3F] text-white'><FaPlus />Add a Friend</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
