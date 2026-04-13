295883638ce158fd5cc45c5ed507a7f7630f8ebc
umayer10-AI <mdumayer10@gmail.com>
2026-04-13 19:53:40 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from '@/component/Banner';
+import Boxes from '@/component/Boxes';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       <Banner></Banner>
+      <Boxes></Boxes>
     </div>
   );
 };
--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div className='text-center py-10 space-y-4'>
+        <div className='text-center py-10 space-y-5 max-w-[80%] mx-auto'>
             <h2 className='text-3xl lg:text-4xl font-bold'>Friends to keep close in your life</h2>
             <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br /> relationships that matter most.</p>
             <div className='flex justify-center items-center'>
