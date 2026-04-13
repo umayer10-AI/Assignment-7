76a3c50b4e3e84cecf207776b254149ed5b987aa
umayer10-AI <mdumayer10@gmail.com>
2026-04-13 20:09:13 +0600

    add

--- src/component/AllCarts.jsx
+++ src/component/AllCarts.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import data from "../../public/data.json"
 
 const AllCarts = () => {
 
     // const res = await fetch("http://localhost:3000/data.json")
     // const data = await res.json()
-    // console.log(data)
+    console.log(data)
 
     return (
         <div>
-            
+            sljgdlkvjhgbetdgdb
         </div>
     );
 };
