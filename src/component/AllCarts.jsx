c2f44e223485fad4ec1251fa538eb1c2d95aec6a
umayer10-AI <mdumayer10@gmail.com>
2026-04-13 20:06:14 +0600

    add

--- src/component/AllCarts.jsx
+++ src/component/AllCarts.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const AllCarts = async () => {
+const AllCarts = () => {
 
-    const res = await fetch("http://localhost:3000/data.json")
-    const data = await res.json()
+    // const res = await fetch("http://localhost:3000/data.json")
+    // const data = await res.json()
     // console.log(data)
 
     return (
