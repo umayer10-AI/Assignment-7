e0699ad20d18325d2f6ef68a33f3d89391dcd7fd
umayer10-AI <mdumayer10@gmail.com>
2026-04-14 09:46:20 +0600

    add

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -9,14 +9,13 @@ const page = () => {
 
             <h2 className='text-4xl font-bold mt-10 mb-5'>Timeline </h2>
 
-            <div className="dropdown dropdown-start">
-                <div tabIndex={0} role="button" className="btn m-1">Filter timeline <MdKeyboardArrowDown className='text-2xl' /></div>
-                <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <div className="dropdown dropdown-start mb-5">
+                <div tabIndex={0} role="button" className="btn m-1 bg-green-700 text-white">Filter timeline <MdKeyboardArrowDown className='text-2xl' /></div>
+                <ul tabIndex="-1" className="dropdown-content menu bg-sky-300 rounded-box z-1 w-52 p-2 shadow-sm">
                     <li><a>Item 1</a></li>
                     <li><a>Item 2</a></li>
                 </ul>
             </div>
-
 
 
             <AllBtnSelector></AllBtnSelector>
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { RiInstagramFill } from 'react-icons/ri';
 
 const Footer = () => {
     return (
-        <div>
-            hrhtdh
+        <div className='bg-[#244D3F] mt-20'>
+            <div className='text-center max-w-[80%] mx-auto space-y-4 pt-10'>
+                <h2 className='text-3xl lg:text-5xl font-bold text-white'>KeenKeeper</h2>
+                <p className='font-semibold text-white'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                <h2 className='text-2xl font-bold text-white'>Social Links</h2>
+                <ul className='flex justify-center items-center gap-3 mb-10'>
+                    <li className='p-2 rounded-full bg-white'><RiInstagramFill /></li>
+                    <li className='p-2 rounded-full bg-white'><FaFacebookSquare /></li>
+                    <li className='p-2 rounded-full bg-white'><FaXTwitter /></li>
+                </ul>
+                <div className='font-semibold py-5 text-gray-400 flex flex-col lg:flex-row gap-4 lg:gap-0 border-t border-t-gray-500 justify-between items-center'>
+                    <p>© 2026 KeenKeeper. All rights reserved.</p>
+                    <ul className='flex items-center gap-5'>
+                        <li>Privacy Policy</li>
+                        <li>Terms of Service</li>
+                        <li>Cookies</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
