dbf39b4c14ea7912ce35e1019e0cb81bb0b0e5be
umayer10-AI <mdumayer10@gmail.com>
2026-04-15 20:59:07 +0600

    add

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,20 +5,16 @@ const NotFound = () => {
     return (
         <div className="flex flex-col items-center justify-center my-20 text-center bg-base-200 px-4">
       
-      {/* 404 Text */}
       <h1 className="text-7xl font-extrabold text-green-800">404</h1>
 
-      {/* Title */}
       <h2 className="text-2xl font-bold mt-4">
         Page Not Found
       </h2>
 
-      {/* Description */}
       <p className="text-gray-500 mt-2 max-w-md">
         Oops! The page you are looking for doesn’t exist or has been moved.
       </p>
 
-      {/* Button */}
       <Link
         href="/"
         className="mt-6 px-6 py-2 rounded-full bg-black text-white hover:bg-gray-800 transition"
